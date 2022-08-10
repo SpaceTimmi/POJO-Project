77fbd66f25c8221e600ce5936d69fc305e3a81c8
Timi Ombe <timmyombe@gmail.com>
2022-08-10 11:06:18 +0100

    Finished 12-array-converter.js

--- problems/12-array-converter.js
+++ problems/12-array-converter.js
@@ -13,7 +13,20 @@ console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {app
 
 function arrayConverter(array) {
   // Your code here
+  let newObj = {}
+  array.forEach(function (element) {
+    if (newObj[element] != undefined) {
+      newObj[element] += 1;
+    } else {
+      newObj[element] = 1
+    }
+  });
+  return newObj
 }
-
+/*
+console.log(arrayConverter(["apple", "apple"])); // => {apple: 2}
+console.log(arrayConverter(["mango", "pineapple"])); // => {mango: 1, pineapple: 1}
+console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {apple: 1, banana: 2, potato: 1}
+*/
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = arrayConverter;
