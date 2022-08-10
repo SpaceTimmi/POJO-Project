784f3d4b49b8df289049b529343cf7afdcd8f763
Timi Ombe <timmyombe@gmail.com>
2022-08-10 11:10:35 +0100

    Finished 13-string-converter.js

--- problems/13-string-converter.js
+++ problems/13-string-converter.js
@@ -13,7 +13,21 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 
 function stringConverter(string) {
   // Your code here
+  let strObj = {};
+  let newStr = string.split(""); 
+  newStr.forEach(function (element) {
+    if (strObj[element] != undefined) {
+      strObj[element] += 1;
+    } else {
+      strObj[element] = 1;
+    }
+  });
+  return strObj;
 }
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = stringConverter;
