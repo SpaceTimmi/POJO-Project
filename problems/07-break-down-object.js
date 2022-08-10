be34d2ecf85546962674ea5967d659b3e7a05e6a
Timi Ombe <timmyombe@gmail.com>
2022-08-10 10:30:30 +0100

    Finished 07-break-down-object.js

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -16,7 +16,16 @@ breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 
 function breakDownObj(obj) {
   // Your code here
+  let result = [ ...Object.keys(obj), ...Object.values(obj) ]
+  console.log(result)
+  return result
 }
+/*
+let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+breakDownObj(object1); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
 
+let object2 = {location: 'NY', borough: 'Brooklyn'};
+breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
+*/
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = breakDownObj;
